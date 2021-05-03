6733af5fc556ba619c994234673b905941674760
fitgeek96 <fitgeek96@gmail.com>
2021-05-03 04:33:55 +0300

    handled auth

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 //jshint esversion:6
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
+
+// Passport Config 
+require('./config/passport')(passport);
+
+
+// Load Routes 
+const auth = require('./routes/auth');
 
 const app = express();
+
+
 
 app.get('/', (req, res) => {
     res.send('Salaaaaaaaaaam ....');
 });
+
+
+app.use('/auth', auth);
 
 
 const port = process.env.PORT || 5000;
---
+++ config/keys.js
@@ -0,0 +1,6 @@
+//jshint esversion:6
+module.exports = {
+    mongoURI: 'mongodb+srv://admin-tahar:leomessi10@cluster0.esdss.mongodb.net/storybooks',
+    googleClientID:'510482194305-9cqhvusfiom3fkm7h8lqme040qpfdrre.apps.googleusercontent.com',
+    googleClientSecret: 'EmONtBM07_LHaP5eEs6w_rVz'
+}
